50dcc64bba340ec9a1169d743292170b9efbffec
sea-lakhan <lakhansagar2695@gmail.com>
2022-11-07 13:32:59 +0530

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header";
 import Home from "./screens/home";
 import Login from "./screens/login";
 import Registration from "./screens/registration";
 function App() {
   return (
-      <>
+    <>
       <Router>
         <Routes>
-        <Route path="/" element={<Header />} exact /> 
-        <Route path="/Login" element={<Login />} exact />
-        <Route path="/Registration" element={<Registration />} exact />
+          <Route path="/" element={<Home />} exact />
+          <Route path="/Login" element={<Login />} exact />
+          <Route path="/Registration" element={<Registration />} exact />
         </Routes>
       </Router>
     </>
-    
   );
 }
-  
-
 
 export default App;
