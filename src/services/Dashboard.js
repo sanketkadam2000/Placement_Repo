879709d21138fbd34a7ecc08e1f682ea666f7a42
sanketkadam2000 <ssk1292000@gmail.com>
2022-11-09 01:24:00 +0530

    Create dashboard

--- src/services/Dashboard.js
+++ src/services/Dashboard.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
 
+import { FaBars, FaUserAlt, FaCertificate, FaUserTie, FaUser, FaUniversity } from "react-icons/fa";
+import { AiTwotoneFileExclamation } from "react-icons/ai";
+import { ImBook } from "react-icons/im";
+import { RiAdminFill } from "react-icons/ri";
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="dashboardContainer">
+      <h1>We've completed</h1>
+      <div className='stats'>
+
+        <div>
+          <FaUserAlt  />
+          <span className='sta' data-max='400'>0</span>
+          <p>Student</p>
+      </div>
+      <div>
+          <RiAdminFill />
+          <span className='sta' data-max='400'>0</span>
+          <p>Admin</p>
+      </div>
+      <div>
+          <FaCertificate />
+          <span className='sta' data-max='400'>0</span>
+          <p>Certificate</p>
+      </div>
+      <div>
+          <FaUniversity />
+          <span className='sta' data-max='400'>0</span>
+          <p>College</p>
+      </div>
+      <div>
+          <AiTwotoneFileExclamation />
+          <span className='sta' data-max='400'>0</span>
+          <p>Placement</p>
+      </div>
+      <div>
+          <FaUserTie />
+          <span className='sta' data-max='400'>0</span>
+          <p>User</p>
+      </div>  
+      </div>
+      </div>
+
+      
   )
 }
 
-export default Dashboard
+export default Dashboard;
